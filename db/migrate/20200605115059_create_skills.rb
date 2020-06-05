2aa241e29eee53d94076e699539ac736c42b0c97
shin-tmd <61884039+shin-tmd@users.noreply.github.com>
2020-06-05 21:08:07 +0900

    skillモデルの作成

---
+++ db/migrate/20200605115059_create_skills.rb
@@ -0,0 +1,12 @@
+class CreateSkills < ActiveRecord::Migration[5.2]
+  def change
+    create_table :skills do |t|
+      t.string  :name,              null: false
+      t.integer :price,             null: false
+      t.string  :description
+      # t.integer :suplier_id
+      # t.integer :consumer_id
+      t.timestamps
+    end
+  end
+end
