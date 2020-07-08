fedc0b71c63d9f06a5235490bcacc59370803c29
sega16322 <aventador_849@docomo.ne.jp>
2020-07-08 10:03:58 +0900

    deviseのカスタム
    rails g devise:controllers usersの実行
    devise関連のルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: "tops#index"
   resources :tops, only: [:index]
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
 end
