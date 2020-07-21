1b8eea59d1318d9cb317dc979f8ac3c6dd129e23
sega16322 <aventador_849@docomo.ne.jp>
2020-07-21 23:48:47 +0900

    gender, birthday, imageを入力可能に変更
    ストロングパラメータの編集
    select_dateのformの記述の変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,13 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
-
+  # def  configure_permitted_parameters
+  #   birth = params[:user]["birth_date(1i)"] + "-" +params[:user]["birth_date(2i)"] + "-" +  params[:user]["birth_date(3i)"]
+  #   params.require(:user).permit(:nickname, :email, :password, :password_confirmation, :birthday, :image, :gender).merge(birth_date: birth)
+  #   # params.require(:user).permit(:nickname, :email, :password, :password_confirmation, :first_name, :last_name, :birth_date)
+  # end
   def configure_permitted_parameters
-    added_attrs = [ :email, :nickname, :password, :password_confirmation, :birthday, :image ]
+    added_attrs = [ :email, :nickname, :password, :password_confirmation, :birthday, :image, :gender ]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
